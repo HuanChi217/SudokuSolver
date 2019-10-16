55c4587e8d982e5e56f8a2fee8baa3f8f8d8b59f
chris <huan-chi.li@go.shoreline.edu>
2019-10-16 14:42:42 -0700

    Sudoku Solver - Second Board Implementation Read different file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main (String[] args) throws FileNotFoundException {
-        Board SDKBoard = new Board("src/main/resources/sudoku.ss");
+        Board SDKBoard = new Board("src/main/resources/sudoku.sdk");
+        Board SSBoard = new Board("src/main/resources/sudoku.ss");
         System.out.println(SDKBoard.toString());
+        System.out.println(SSBoard.toString());
     }
 }
