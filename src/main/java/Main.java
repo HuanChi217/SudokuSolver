bfaa81a4e09998cc6aaa34e2ab44e01c2d19c531
chris <huan-chi.li@go.shoreline.edu>
2019-10-16 13:27:12 -0700

    Sudoku Solver - Second Board Implementation Read different file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main (String[] args) throws FileNotFoundException {
-        Board SDKBoard = new Board("src/main/resources/sudoku.sdk");
+        Board SDKBoard = new Board("src/main/resources/sudoku.ss");
         System.out.println(SDKBoard.toString());
     }
 }
--- src/main/java/SSBoardReader.java
+++ src/main/java/SSBoardReader.java
@@ -6,12 +6,19 @@ public class SSBoardReader implements BoardReader{
     public char[][] boardBuilder(String filepath) throws FileNotFoundException {
         Scanner scan = new Scanner(new File(filepath));
         char[][] element = new char[9][9];
+        int row = 0;
+        int col;
         for (int i = 0; i < 11 ; i++) {
             String line = scan.nextLine();
+            col = 0;
             for (int j = 0; j < 11; j++) {
                 if(line.charAt(j) != '|' && line.charAt(j) != '-'){
-                    element[i][j] = line.charAt(j);
+                    element[row][col] = line.charAt(j);
+                    col++;
                 }
+            }
+            if (!line.contains("-")) {
+                row++;
             }
         }
         return element;
